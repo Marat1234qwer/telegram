32fbe4067e812bfe2d38d353b84be998c7443212
Marat1234qwer <119105650+Marat1234qwer@users.noreply.github.com>
2023-11-29 18:42:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,87 +1,25 @@
-from flask import Flask, request
-import requests
-from dotenv import load_dotenv
-import os
-from os.path import join, dirname
-from yookassa import Configuration, Payment
-import json
+# from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-app = Flask(__name__)
+import time
 
-
-def create_invoice(chat_id):
-    Configuration.account_id = get_from_env("SHOP_ID")
-    Configuration.secret_key = get_from_env("PAYMENT_TOKEN")
-
-    payment = Payment.create({
-        "amount": {
-            "value": "100.00",
-            "currency": "RUB"
-        },
-        "confirmation": {
-            "type": "redirect",
-            "return_url": "https://google.com"
-        },
-        "capture": False,
-        "description": "Заказ №1",
-        "metadata": {"chat_id": chat_id}
-    })
-
-    return payment.confirmation.confirmation_url
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options = option)
 
 
-def get_from_env(key):
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-    return os.environ.get(key)  # возвращен серкетный токен (или ключ к платежной системе)
+try:
+    driver.get("https://www.marinetraffic.com/en/ais/details/ships/shipid:753444/mmsi:636012382/imo:9299692/vessel:NS_CONCORD")
+    time.sleep(2)
+    
 
 
-def send_message(chat_id, text):
-    method = "sendMessage"
-    token = get_from_env("TELEGRAM_BOT_TOKEN")
-    url = f"https://api.telegram.org/bot{token}/{method}"
-    data = {"chat_id": chat_id, "text": text}
-    requests.post(url=url, data=data)
-
-
-def send_pay_button(chat_id, text):
-    invoice_url = create_invoice(chat_id)
-
-    method = "sendMessage"
-    token = get_from_env("TELEGRAM_BOT_TOKEN")
-    url = f"https://api.telegram.org/bot{token}/{method}"
-
-    data = {"chat_id": chat_id, "text": text, "reply_markup": json.dumps({"inline_keyboard": [[{
-        "text": "Оплатить!",
-        "url": f"{invoice_url}"
-    }]]})}
-
-    requests.post(url, data=data)
-
-
-def check_if_successful_payment(request):
-    try:
-        if request.json["event"] == "payment.succeeded":
-            return True
-    except KeyError:
-        return False
-
-    return False
-
-
-@app.route('/', methods=["POST"])  # localhost:5000/ - на этот адрес телеграм шлет свои сообщение
-def process():
-    if check_if_successful_payment(request):
-        # Обработка запроса от Юкассы
-        chat_id = request.json["object"]["metadata"]["chat_id"]
-        send_message(chat_id, "Оплата прошла успешно")
-    else:
-        # Обработка запроса от Телеграм
-        chat_id = request.json["message"]["chat"]["id"]
-        send_pay_button(chat_id=chat_id, text="Тестовая оплата")
-
-    return {"ok": True}
-
-
-if __name__ == '__main__':
-    app.run()
+    print(driver.page_source)
+    # python3 main.py > test.txt
+    time.sleep(5)
+    
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
